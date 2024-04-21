fa16f3d6ba0a64d64f6c20d5952b639a590b45f9
Viktoroyz <126620500+Viktoroyz@users.noreply.github.com>
2024-04-21 21:32:03 +0200

    Закончил_с_header_Начал_блок_about

--- dior/js/main.js
+++ dior/js/main.js
@@ -1,6 +1,21 @@
-const swiper = new Swiper('.swiper', {
+const swiperTop = new Swiper('.top__swiper', {
   // Optional parameters
-  loop: true,
+  effect: 'fade',
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+const swiperAbout = new Swiper(".about__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
